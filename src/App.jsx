62afc24b86048f686c16d14c70e801ea91d4f3bd
Rashi Dhar <rdhar1997@gmail.com>
2020-11-17 23:03:30 -0500

    feat: seperate card, add to cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import 'fontsource-roboto';
 import axios from 'axios';
 import {Card, Typography, Link } from '@material-ui/core';
 import Chip from '@material-ui/core/Chip';
-// import data from './data.js';
+import data from './data.js';
 import { GoRepoForked, GoStar, GoLinkExternal} from "react-icons/go";
+import Repo from './components/Repo'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -15,84 +16,58 @@ export default class App extends React.Component {
       isLoaded: false,
       data: [],
       cart: 0,
-      searchname: ''
+      searchname: 'jquery'
     }
     this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
-    this.fetchSearchResults("");
+    this.fetchSearchResults(this.state.searchname);
   }
 
   displayItems = (repo) => {
-    return <Card style={{width:300, margin: 10}}>
-      <Typography variant="h5" style={{margin: 10}}>
-        {repo.name}
-        <Link href={repo.html_url}>
-          <GoLinkExternal size={24}/>          
-        </Link> 
-      </Typography>
-      <Chip
-        size="small"
-        style={{margin: 10}}
-        icon={<GoRepoForked size={24} />}
-        label={repo.forks_count}
-        color="default"
-      />
-      <Chip
-        size="small"
-        color="default"
-        style={{margin: 10}}
-        icon={<GoStar color="yellow" size={24} />}
-        label={repo.stargazers_count}
-      />
-      
-      <Typography style={{margin: 10}}>
-        {repo.description}
-      </Typography>
-      
-    </Card>
+    return <Repo repo={repo}></Repo>
   }
 
   fetchSearchResults = async (query) => {
-    if(query){
-      const repoUrl = `https://api.github.com/search/repositories?q=${query}+in:name,description&sort=stars&order=desc`;
-      axios.get(repoUrl).then((response) => {
-        const repos = response.data.items
-        this.setState({
-          isLoaded: true,
-          data: repos
-        });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-      })
-    }
-    else {
-
-      this.setState({
-        isLoaded: true,
-        data: []
-      });
-    }
-    // data(`"${query} in:name,description"`)
-    //   .then(
-    //     (result) => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         data: result.search.edges
-    //       });
+    // if(query){
+    //   const repoUrl = `https://api.github.com/search/repositories?q=${query}+in:name,description&sort=forks&order=desc`;
+    //   axios.get(repoUrl).then((response) => {
+    //     const repos = response.data.items
+    //     this.setState({
+    //       isLoaded: true,
+    //       data: repos
+    //     });
     //     },
     //     (error) => {
     //       this.setState({
     //         isLoaded: true,
     //         error
     //       });
-    //     }
-    //   )
+    //   })
+    // }
+    // else {
+
+    //   this.setState({
+    //     isLoaded: true,
+    //     data: []
+    //   });
+    // }
+    data(`"${query} in:name,description"`)
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result.search.edges
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   handleChange(event) {
@@ -108,13 +83,13 @@ export default class App extends React.Component {
     } else {
       return (
         <div className="App">
-          <Typography variant="h4">Look for top 10 GitHub repositories!</Typography>
+          <Typography variant="h4">Look for top GitHub repositories!</Typography>
           <form onSubmit={this.handleSubmit}>
             <label>
               Keyword:
               <input type="text" value={this.state.searchname} onChange={this.handleChange} />
             </label>
-          </form>
+          </form><br></br>
           <div className="flowers">
             {data.map(this.displayItems)}
           </div>
--- src/components/Repo.jsx
+++ src/components/Repo.jsx
@@ -1,25 +1,70 @@
 import React, { Component } from "react";
-import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import {Card, Typography, Link, Box, CardMedia } from '@material-ui/core';
+import Chip from '@material-ui/core/Chip';
+import { GoRepoForked, GoStar, GoLinkExternal} from "react-icons/go";
 
 
 class Repo extends Component {
+
+  changeDate = (date) => {
+    const today = new Date(date);
+    const options = {  year: 'numeric', month: 'long', day: 'numeric' };
+    return today.toLocaleDateString(undefined, options);
+  }
   render() {
-    const repos = this.props.repodata;
+    const repos = this.props.repo.node;
     return (
-      <Card>
-        <CardContent>
-        <Typography color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-      </Card>
+      <Card style={{width:300, margin: 10}}>
+      <Typography variant="h4" style={{margin: 10}}>
+        {repos.name}
+      </Typography>
+      <Typography variant="h8" style={{margin: 10}}>
+        <Link href={repos.url}>
+        {repos.nameWithOwner}
+          <GoLinkExternal size={16}/>          
+        </Link> 
+      </Typography>
+
+      <br></br>
+      <Chip
+        size="small"
+        style={{margin: 10}}
+        icon={<GoRepoForked size={24} />}
+        label={repos.forkCount}
+        color="default"
+      />
+      <Chip
+        size="small"
+        color="default"
+        style={{margin: 10}}
+        icon={<GoStar color="yellow" size={24} />}
+        label={repos.stargazers.totalCount}
+      />
+      <br></br>
+      <Typography variant="h5" style={{margin: 10}}>
+        {repos.description}
+      </Typography>
+      <Typography variant="h8" style={{margin: 10}}>
+        <Box fontWeight="fontWeightBold">
+        {(repos.primaryLanguage)?repos.primaryLanguage.name:""}
+        
+        </Box>
+      </Typography>
+      <Typography variant="h7" style={{margin: 10}}>
+        <Box fontWeight="fontWeightLight" fontFamily="Monospace">
+      By: <Link href={repos.owner.url}><img src={repos.owner.avatarUrl} alt="card-img" height="30" width="30"></img></Link>
+        </Box>
+      </Typography>
+      <Typography variant="h7" style={{margin: 10}}>
+        <Box fontWeight="fontWeightLight" fontFamily="Monospace">
+         Last updated: {this.changeDate(repos.updatedAt)}
+        </Box>
+      </Typography>
+      
+    </Card>
     );
   }
 }
